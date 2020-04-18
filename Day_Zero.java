b445369ab6fb49a8f604ac3d0cb1c31653b7fbe2
Aedgen <38895858+zIDAedgen@users.noreply.github.com>
2020-04-19 07:27:19 +0900

    Update Day_Zero.java

--- Day_Zero.java
+++ Day_Zero.java
@@ -1,1 +1,61 @@
 
+/*
+
+
+
+$Introduction
+
+
+    P2 : Microservices
+
+    The single-app : a application that contains multiple models, including code and pages. This app will be assembled as
+    a WAR package. The 
+
+    Microservices : Modulization of the components, which will create a network-structured application 
+
+    Spring Boot ---network-structured--> Spring Cloud --- data flow between the components ---> Spring Cloud Data Flow
+  
+
+    P3 : Building
+    
+    
+$Hello World
+       1. initialize an app
+       2.import starters
+       3.create main app
+       4.run the project
+  
+  
+  
+    P4 : 3 annotation and jar-package
+          1. @SpringBootApplication : the main interface of the project (There will be a specification at P6)
+          2. @Controller -- get or post the data via http
+          3. @ResponseBody  --- return the data to broser
+          * @Controller + @ResponseBody = @RestController
+          
+          
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    Spring Boot @Controller --- 3 annotation and jar package
+      @
+  
+  
+  
+*/
